064d6e936216cb18f7cac24ffe442536ea65ba84
Dmitrii <sorokindm1try@yandex.ru>
2025-04-13 11:42:08 +0300

    SpringMvcApp1 2.1

--- src/main/java/ru/sorokin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/sorokin/springcourse/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.sorokin.springcourse.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam(value = "surname",required = false) String surname,
+                            Model model) {
+      //  System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("massage", "Hello " + name + " " + surname);
 
         return "first/hello";
 
