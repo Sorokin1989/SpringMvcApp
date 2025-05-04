4695e6291dea6e270ffad4a246060346d1023fb9
Dmitrii <sorokindm1try@yandex.ru>
2025-05-04 15:01:05 +0300

    SpringMvcApp1 2.9

--- src/main/java/ru/sorokin/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/sorokin/springcourse/controllers/PeopleController.java
@@ -3,10 +3,7 @@ package ru.sorokin.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.sorokin.springcourse.dao.PersonDAO;
 import ru.sorokin.springcourse.models.Person;
 
@@ -40,6 +37,16 @@ public String create(@ModelAttribute("person") Person person) {
     personDAO.save(person);
     return "redirect:/people";
 
+}
+@GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+model.addAttribute("person",personDAO.show(id));
+return "people/edit";
+}
+@PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    personDAO.update(id,person);
+    return "redirect:/people";
 
 }
 
