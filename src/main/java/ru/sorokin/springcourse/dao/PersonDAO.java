0bd9befc299780e754b25294ac990af1590174a2
Dmitrii <sorokindm1try@yandex.ru>
2025-05-06 14:41:12 +0300

    SpringMvcApp1 CRUD 2.1

--- src/main/java/ru/sorokin/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/sorokin/springcourse/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package ru.sorokin.springcourse.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.sorokin.springcourse.models.Person;
@@ -21,44 +22,24 @@ public class PersonDAO {
 
 
     public List<Person> index() {
-        return jdbcTemplate.query("select * from person", new PersonMapper());
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("select * from person where id = ?", new Object[]{id}, new PersonMapper())
+        return jdbcTemplate.query("select * from person where id = ?", new Object[]{id},
+                        new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
+        jdbcTemplate.update("insert into person values(1,?,?,?)", person.getName(), person.getAge(),
+                person.getEmail());
 
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into person values(1,?,?,?)");
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2, person.getAge());
-            preparedStatement.setString(3, person.getEmail());
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void update(int id, Person updatedPerson) {
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("update Person set name=?, age=?," +
-                    "email=? where id=?");
-
-            preparedStatement.setString(1, updatedPerson.getName());
-            preparedStatement.setInt(2, updatedPerson.getAge());
-            preparedStatement.setString(3, updatedPerson.getEmail());
-            preparedStatement.setInt(4, id);
-            preparedStatement.executeUpdate();
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+jdbcTemplate.update("update Person set name=?, age=?, email=? where id=?", updatedPerson.getName(),
+        updatedPerson.getAge(), updatedPerson.getEmail(), id);
 
     }
 
